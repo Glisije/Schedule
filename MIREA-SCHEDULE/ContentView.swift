fc36c51e78903a194bd81ed940e0e38740c37b34
Romas <glisije@yandex.ru>
2021-04-02 22:17:26 +0300

    Initial Commit

---
+++ MIREA-SCHEDULE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MIREA-SCHEDULE
+//
+//  Created by Ромас Буткус on 02.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MIREA-SCHEDULE/MIREA_SCHEDULEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIREA_SCHEDULEApp.swift
+//  MIREA-SCHEDULE
+//
+//  Created by Ромас Буткус on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MIREA_SCHEDULEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
