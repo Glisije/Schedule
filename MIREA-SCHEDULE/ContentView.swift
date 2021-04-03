76b7110057f4595a036b7d8a020a232361880908
Romas <glisije@yandex.ru>
2021-04-03 12:06:25 +0300

    Learning pt1

--- MIREA-SCHEDULE/ContentView.swift
+++ MIREA-SCHEDULE/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack(alignment: .center){
+//            Text("Привет, студент!")
+//                .font(.largeTitle)
+//                .bold()
+//                .padding(10)
+//                .background(Color.init(red: 0, green: 0.76, blue: 1))
+//                .cornerRadius(15)
+//        }
+//    }
+//}
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    Image("LOGO")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+        .background(Color("yellow"))
+        
+//    Image(systemName: "person.3.fill")
+//        .font(.largeTitle)
+//        .padding()
+//        .background(Color .red)
+//        .cornerRadius(15)
+    
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
